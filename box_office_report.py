801dafb6a7e357dd518860830cc050797d7a2a07
StefanoTamayo <tamayo.stefano@student.ccm.edu>
2026-09-16 11:28:56 -0400

    Finished
    Updated print statements to format monetary values with commas and two decimal places.

--- box_office_report.py
+++ box_office_report.py
@@ -1,15 +1,22 @@
+#Stefano Tamayo
+#CMP-131-80230
+#Week 4
+#Lab 2
+#Box Office Report
+#9/16/26
+
 name=input("Name of Movie: ")
 num1=int(input("How many adult tickets were sold? "))
 num2=int(input("How many child tickets were sold? "))
-num1=num1*10
-num2=num2*6
-numG=num1+num2 
+num11=num1*10
+num22=num2*6
+numG=num11+num22
 numN=numG*.2
 amount=numG-numN
 print()
 print("Movie Name: ",name)
 print("Adult Tickets Sold: ",num1)
 print("Child Tickets Sold: ",num2)
-print("Gross Box Office Profit: $",numG)
-print("Net Box Office Profit: $",numN)
-print("Amount Paid to Distributor: $",amount)
+print(f"Gross Box Office Profit: ${numG:,.2f}")
+print(f"Net Box Office Profit: ${numN:,.2f}")
+print(f"Amount Paid to Distributor: ${amount:,.2f}")
