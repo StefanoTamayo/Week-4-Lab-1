423f3ec65928dd4d350a47983bf4fe8d78bcf934
StefanoTamayo <tamayo.stefano@student.ccm.edu>
2026-09-16 23:45:44 -0400

    Finished

--- interest_earned.py
+++ interest_earned.py
@@ -1,13 +1,20 @@
+#Stefano Tamayo
+#CMP-131-80230
+#Week 4
+#Lab 2
+#Box Office Report
+#9/16/26
+
 principal=float(input("What is the principal amount? "))
 interest=float(input("What is the interest rate? "))
 rate=interest/100
 compound=float(input("How many times compounded? "))
 amount=principal*((1+(rate/compound))**compound)
-amount=amount-principal
-Interest_Amount=rate*principal
+Interest_Amount=amount-principal
+savings=Interest_Amount+principal
 print()
 print("Interest Rate:",interest,"%")
 print("Times Compounded:",compound)
-print("Principal:",principal)
-print("Interest:",Interest_Amount)
-print("Amount in Savings:",amount)
+print(f"Principal: ${principal:,.2f}")
+print(f"Interest: ${Interest_Amount:,.2f}")
+print(f"Amount in Savings: ${savings:,.2f}")
